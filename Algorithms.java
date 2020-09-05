8e21c7c3dc6a2ddade5dd48c40a6b60f5b1834e0
whxue0 <whxue0@163.com>
2020-09-05 22:06:58 +0800

    nothing

--- Algorithms.java
+++ Algorithms.java
@@ -7,30 +7,26 @@ public class Algorithms {
 
 
     /**
-     * 据顶点列表和边的权值表，找出从开始位置到结束位置的最短路径
+     * 跟据权值表，找出从开始位置到结束位置的最短路径
      * 且途中需要经过pass_places里面的地点，需要按顺序返回经过的各个顶点序号
      * @param edges_weight  权重表
      * @param start 开始顶点序号
      * @param end   结束结点序号
      * @param pass_places   途径结点序号
-     * @return
+     * @return 按顺序返回一个列表表示路径（包含start 和 end）
+     *
+     *     小明想从地点start去到地点end，但是他还要途经多个顶点，请帮小明找出一条最短路径吧。
+     *     pass_places是小明途径的地点列表,start、end、pass_places中的地点不会重复。
+     *     edges_weight[i][j] 表示地点i到地点j的距离，edges_weight[i][j] == edges_weight[j][i]，
+     * 且edges_weight[i][i]==0,edges_weight[i][j] == -1 表示两点不直接可达。
+     *     结果需要返回小明需要的最短路径列表（返回的列表需要包含start和end）
+     *
      */
-
     public static ArrayList<Integer> findShortestPath(int[][] edges_weight,
                                                       int start,
                                                       int end,
                                                       ArrayList<Integer> pass_places ){
-        ArrayList<Integer> test_path = new ArrayList<>();
-        test_path.add(1);
-        test_path.add(2);
-        test_path.add(3);
-        test_path.add(10);
-        test_path.add(17);
-        test_path.add(19);
-        test_path.add(20);
-        test_path.add(21);
-
-        return test_path;
+        return new ArrayList<Integer>();
     }
 
 }
